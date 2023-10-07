5d208ec91ae31a769674106f6f87db88ff8e36fc
Rafael Maximo <rafael.ribeiro@faculdadeimpacta.com.br>
2023-10-07 12:07:29 -0300

    altera o método permitido pra post, adiciona objeto de request e exibe os dados recebidos com pprint

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
+from pprint import pprint
+
+from flask import Flask, request
 
 app = Flask(__name__)
 
+# a definição do método post faz com que essa rota não responda mais com get, ou seja,
+# tentar acessá-la no navegador irá resultar em 405 - método não permitido
 
-@app.route('/cadastro')
+@app.route('/cadastro', methods=['post'])
 def cadastro():
+    # objeto request é criado pelo flask e contém todos os dados da requisição recebida pelo servidor
+    pprint(vars(request))
+    # request.form é um dicionário com os valores que foram enviados pelo formulário
+    pprint(request.form)
     return 'ok', 200
 
 
